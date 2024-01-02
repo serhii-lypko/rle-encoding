cf8f1d9891e3f83f91ac2fd1197b9c4d350f9653
Serhii Lypko <lypko.sg@gmail.com>
2024-01-02 19:41:24 +0700

    Delete rust/target directory

--- rust/target/debug/deps/rust-2d97ee9022fa9e06.d
+++
@@ -1,5 +0,0 @@
-/Users/serhiilk/Playgrounds/rle/rust/target/debug/deps/librust-2d97ee9022fa9e06.rmeta: src/main.rs
-
-/Users/serhiilk/Playgrounds/rle/rust/target/debug/deps/rust-2d97ee9022fa9e06.d: src/main.rs
-
-src/main.rs:
